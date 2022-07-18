1c625e657adbb210f8cc84ab6a29285aa0a95dfd
Veronika Sirak <7nicka@gmail.com>
2022-07-18 17:58:07 +0100

    refactor API

--- src/services/contactsAPI.js
+++ src/services/contactsAPI.js
@@ -2,27 +2,29 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://62d1579bd4eb6c69e7db740e.mockapi.io';
 
-const fetchContactsfromPhonebook = axios.create({
-  baseURL: 'https://62d1579bd4eb6c69e7db740e.mockapi.io',
-  method: 'get',
-});
-
 export const fetchContacts = async () => {
-  const { data } = await fetchContactsfromPhonebook('/contacts');
-  return data;
+  try {
+    const { data } = await axios.get('/contacts');
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-// const addContactsfromPhonebook = axios.create({
-//   baseURL: 'https://62d1579bd4eb6c69e7db740e.mockapi.io',
-//   method: 'post',
-// });
-
 export const addContacts = async contact => {
-  const { data } = await axios.post('/contacts', contact);
-  return data;
+  try {
+    const { data } = await axios.post('/contacts', contact);
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const deleteContacts = async id => {
-  const { data } = await axios.delete(`/contacts/${id}`);
-  return data;
+  try {
+    const { data } = await axios.delete(`/contacts/${id}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
