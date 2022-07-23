45523d0a88b96625343f8ca39c39deab3915a24e
nika-sirak <60103912+nika-sirak@users.noreply.github.com>
2022-07-23 02:30:44 +0000

    Deploying to gh-pages from @ nika-sirak/goit-react-hw-08-phonebook@2c419796597dcc7b85fcbd0b72a3b601ad2992b4 🚀

---
+++ static/js/674.c707720f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[674],{8674:function(e,t,a){a.r(t),a.d(t,{default:function(){return r}});var n="HomePage_homePageSection__CNDJ4",o="HomePage_title__UFLA3",c=a(184);var r=function(){return(0,c.jsx)("section",{className:n,children:(0,c.jsx)("h1",{className:o,children:"Phonebook"})})}}}]);
+//# sourceMappingURL=674.c707720f.chunk.js.map
